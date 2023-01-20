b89334f4213df649fc6e5aec5b648e900b28987f
Lidia Bhogal <lidiapriano@lidias-iMac.local>
2023-01-20 20:00:21 +0000

    columns and rows added to html

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,23 @@
-var today = moment();
+$(document).ready(function () {
+
+  var today = moment();
 $("#currentDay").text(today.format("dddd Do MMMM"));
+//  add time
+
+$('.saveBtn').on('click', function (){
+  console.log('button clicked');
+  // save what user enters in text area
+
+  let previousHour;
+  let currentHour;
+  let futureHour;
+
+  if currenttime <
+  //  if time is less than present time use colou red etc etc 
+
+})
+})
 
 
-$( function() {
-    $( ".container" ).selectable();
-    
-  } );
   
   
