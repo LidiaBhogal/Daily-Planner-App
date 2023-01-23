8e7d0361d742158677dbe29adc80b1b5d10ec6fb
Lidia Bhogal <lidiapriano@lidias-iMac.local>
2023-01-23 19:14:51 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Daily-Planner-App
+
+## Description
+
+The app should:
+
+Display the current day at the top of the calendar when a user opens the planner.
+
+Present timeblocks for standard business hours when the user scrolls down.
+
+Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+Allow a user to enter an event when they click a timeblock.
+
+Save the event in local storage when the save button is clicked in that timeblock.
+
+Persist events between refreshes of a page.
+
+## Installation
+
+N/A
+
+## Usage
+
+In this task I used for loops and if statements as well as variables. Below is an image the page once you inspect/console.
+
+
+<img width="2240" alt="Screenshot 2023-01-23 at 19 04 36" src="https://user-images.githubusercontent.com/116956128/214127640-f63c8a63-f9ea-4f2d-a134-07622129ebb2.png">
+
+## Deployment
+
+If you would like to see the finished website please click on the link below
+
+https://lidiabhogal.github.io/Daily-Planner-App/
+
+## License
+
+N/A
+
+## Credits
--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  
+
   var today = moment();
   $("#currentDay").text(today.format("dddd Do MMMM"));
 
@@ -9,78 +9,49 @@ $(document).ready(function () {
   $('.saveBtn').on('click', function () {
     console.log('button clicked');
 
-   
+
   })
 
-console.log($(".description"))
+  console.log($(".description"))
 
-// save what user enters in text area
-  let inputedData =$('.description')
-  let planner =$('#planner')
+  // save what user enters in text area
+  let inputedData = $('.description')
+  let planner = $('#planner')
   let saveButton = $('.saveBtn')
 
   for (let i = 0; i < inputedData.length; i++) {
     inputedData[i].value = localStorage.getItem(`hour${i}`);
-     if (time.format("h") <= (i+9) ) {
+    if (time.format("h") <= (i + 9)) {
       inputedData[i].classList.add('past')
-     } else if (time.format('h') === (i+9)){
+    } else if (time.format('h') === (i + 9)) {
       inputedData[i].classList.add('present')
-     } else (time.format('h') >= (i+9)){
-      inputedData[i].classList.add('future')
-     }
+    } else (time.format('h') >= (i + 9))
+    inputedData[i].classList.add('future')
   }
-}
-
-  function savePlannerInfo(event) {
-    event.preventDefault();
-    console.log(event)
-    let addedPlannerInfo = $('textarea');
-    let textAdded = inputedData.val();
-    // addedPlannerInfo.text(textAdded)
-    // planner.append(addedPlannerInfo)
-    // $(inputedData.val(''))
-    for (let i = 0; i < inputedData.length; i++) {
-      console.log(`this loop has run ${i} times`);
-      console.log("this loop has run " + i + " times" );
-      localStorage.setItem(`hour${i}`, inputedData[i].value);
-    
-    }
-
-   
-  }
-  saveButton.on('click', savePlannerInfo)
-  // write an if statement so the the block highlights in the correct colour
- 
-
-  $('.description').focus(function(){
-    if ( beforeTime < time){
-      let beforeTime = $('this').css('background-color', '#d3d3d3')
-    } else if (presentTime === time){
-      let presentTime = $('this').css('background-color', 'ff6961')
-  
-  } else {( futureTime > time)
-    let futureTime = $('this').css('background-color', '#77dd77',)
-    }
-    }
-)
 })
 
-  // save to localStorage
-  
-  // localStorage.setItem(addedPlannerInfo,textAdded);
 
-  // make sure things are saved once page is refreshed
+function savePlannerInfo(event) {
+  event.preventDefault();
+  console.log(event)
+  let addedPlannerInfo = $('textarea');
+  let textAdded = inputedData.val();
 
-  // sort out the hightlight button
+  for (let i = 0; i < inputedData.length; i++) {
+    console.log(`this loop has run ${i} times`);
+    console.log("this loop has run " + i + " times");
+    localStorage.setItem(`hour${i}`, inputedData[i].value);
+
+  }
+
+  saveButton.on('click', savePlannerInfo)
+}
 
 
 
 
-  
-
-  
 
 
-// refreshButtonEl.on('click', function () {
-//   location.reload();
-// });
+
+
+
